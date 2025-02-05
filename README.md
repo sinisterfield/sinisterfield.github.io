def9dda3994e0e97d9f109067ca39a328d74b2d3
Brig Lamoreaux <briglx@users.noreply.github.com>
2025-02-05 16:07:50 +0000

    update images

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sinister Field
+
+
+Lastest experiments, tools and services from Lamoreaux Labs.
+
+# Quick Start Guide
+
+```bash
+# Clone the project
+
+# Run site locally
+npm run dev
+```
---
+++ src/app/layout.tsx
@@ -0,0 +1,58 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Geologica} from "next/font/google";
+import "./globals.css";
+
+
+const geologicaSans = Geologica({
+  variable: "--font-geologica-sans",
+  subsets: ["latin"],
+});
+
+// const geologicaMono = Geologica_Mono({
+//   variable: "--font-geologica-mono",
+//   subsets: ["latin"],
+// });
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+
+
+export const metadata: Metadata = {
+  title: "Sinister Field",
+  description: "Inspired by left-field creativity, we deliver innovative solutions tailored to your unique needs.",
+  icons: {
+    icon: [
+      {url: 'favicon.ico', sizes: '32x32'},
+      {url: '/icon-16.png', sizes: '16x16', type: 'image/png'},
+      {url: '/icon-32.png', sizes: '32x32', type: 'image/png'},
+      {url: '/icon-48.png', sizes: '48x48', type: 'image/png'},
+      {url: '/icon-192.svg', type: 'image/svg+xml'},
+    ],
+    apple: [{url: '/apple-touch-icon.png', sizes: '180x180'}],
+  },
+  manifest: "/manifest.webmanifest",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geologicaSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
